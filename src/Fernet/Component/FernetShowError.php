fc5c5874eee8955288c60447b187c2f6fb87d4e7
Rodrigo Arce <albo@pragmore.com>
2021-01-19 01:45:46 -0300

    Improve error showing

--- src/Fernet/Component/FernetShowError.php
+++ src/Fernet/Component/FernetShowError.php
@@ -4,18 +4,71 @@ declare(strict_types=1);
 
 namespace Fernet\Component;
 
+use Fernet\Framework;
 use Throwable;
 
 class FernetShowError
 {
+    private const TITLE = 'Error on Fernet';
     public Throwable $error;
     public bool $preventWrapper = true;
+    private string $rootPath;
+
+    public function __construct(Framework $fernet)
+    {
+        $this->rootPath = rtrim($fernet->getOption('rootPath'), DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR;
+    }
+
+    private function path(string $path, int $line): string
+    {
+        $fullPath = $path;
+        if (0 === strpos($path, $this->rootPath)) {
+            $path = substr($path, strlen($this->rootPath));
+        }
+
+        return "File <strong><a href=\"subl://{$fullPath}:{$line}\">{$path}</a></strong> on line: {$line}";
+    }
 
     public function __toString(): string
     {
-        // TODO Improve how the errors are showed
-        $dump = var_export($this->error, true);
-
-        return "<html lang=\"en\"><body><h1>Fernet - Error</h1><pre>$dump</pre></body></html>";
+        ob_start(); ?>
+        <html lang="en">
+        <head>
+            <title><?php echo self::TITLE; ?>:  <?php echo $this->error->getMessage(); ?></title>
+            <FernetFavicon></FernetFavicon>
+            <FernetStylesheet />
+        </head>
+        <body class="show-error">
+            <h1>
+                <div class="icon baseline">
+                    <FernetLogo height="50" width="50" />
+                </div>
+                <?php echo self::TITLE; ?>
+            </h1>
+            <h2><?php echo $this->error->getMessage(); ?></h2>
+            <p>
+                <?php echo $this->path($this->error->getFile(), $this->error->getLine()); ?>
+            </p>
+            <h3>Stack trace</h3>
+            <ol>
+            <?php foreach ($this->error->getTrace() as $trace) { ?>
+                <li>
+                    <p><strong><?php echo $trace['class'] ?? ''; ?><?php echo $trace['type'] ?? ''; ?><?php echo $trace['function'].'()' ?? ''; ?></strong></p>
+                    <p><?php echo $this->path($trace['file'], $trace['line']); ?></p>
+                </li>
+            <?php } ?>
+            </ol>
+            <?php foreach (['ENV', 'SERVER', 'POST', 'GET', 'SESSION'] as $var) { ?>
+            <h3><?php echo $var; ?></h3>
+            <ul>
+            <?php foreach ($GLOBALS["_$var"] as $key => $value) { ?>
+                <li><strong><?php echo $key; ?></strong> <?php echo var_export($value, true); ?></li>
+            <?php } ?>
+            </ul>
+            <?php } ?>
+        </body>
+        </html>
+<?php
+        return ob_get_clean();
     }
 }
