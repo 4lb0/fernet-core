768628ec17c6478f56f90836eed7d498a6f9305b
Rodrigo Arce <albo@pragmore.com>
2021-04-15 00:14:55 -0300

    Fix Link component

--- src/Fernet/Component/Link.php
+++ src/Fernet/Component/Link.php
@@ -23,21 +23,21 @@ class Link
      */
     public function __toString(): string
     {
-        [$component, $method] = explode('.', $this->to);
+        [$component, $method] = explode('.', $this->to.'.');
         if (!$method) {
             $method = 'route';
         }
         $link = $this->routes->get($component, $method, $this->params);
         $isActive = $this->request->server->get('REQUEST_URI') === $link;
-        $css = '';
-        if ($isActive || $this->css) {
-            $classes = [$this->css, '__fm'];
-            if ($isActive) {
-                $classes[] = 'active';
-            }
-            $css = ' class="'.implode(' ', $classes).'"';
+        $classes = ['__fl'];
+        if ($this->css) {
+            $classes[] = $this->css;
         }
+        if ($isActive) {
+            $classes[] = 'active';
+        }
+        $css = implode(' ', $classes);
 
-        return "<a href=\"$link\"$css>$this->childContent</a>";
+        return "<a href=\"$link\" class=\"$css\">$this->childContent</a>";
     }
 }
